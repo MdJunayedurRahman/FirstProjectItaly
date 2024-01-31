f3d81cec8dbcba0a0ee8e9034ebef0461c7d6c37
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-01-31 03:27:34 +0200

    style changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function Header () {
     return (
-        <div id="header"> Civil and industrial cleaning. Painting, clearing of cellars and warehouses. Move. </div>
+        <div id="header"> Civil and industrial cleaning. Painting, clearing of cellars and warehouses. Move.
+         </div>
     )
 }
