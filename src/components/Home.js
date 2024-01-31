b39392993f254c043544b18bc4f1975aa2d2fe4a
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-01-31 11:20:47 +0200

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,74 +1,20 @@
 import React from 'react';
+import {FaGlobe} from 'react-icons/fa'
+import { IconContext } from "react-icons";
 
 export default function Home () {
     return (
         <div class="container-fluid">
-           This is home page.
-
-           Contents hide
-(Top)
-Etymology
-Description
-Prehistory
-Historiography
-Methods
-Toggle Methods subsection
-Areas of study
-Toggle Areas of study subsection
-Historians
-Judgement
-Pseudohistory
-Teaching
-Toggle Teaching subsection
-See also
-References
-Further reading
-External links
-History
-
-Article
-Talk
-Read
-View source
-View history
-
-Tools
-Page semi-protected
-From Wikipedia, the free encyclopedia
-This article is about the academic discipline. For a general history of human beings, see Human history. For other uses, see History (disambiguation).
-
-Herodotus (c. 484 – c. 425 BC) has been considered the "father of history" in the Western world.
-History (derived from Ancient Greek ἱστορία (historía) 'inquiry; knowledge acquired by investigation')[1] is the systematic study and documentation of the human past.[2][3]
-
-The period of events before the invention of writing systems is considered prehistory.[4] "History" is an umbrella term comprising past events as well as the memory, discovery, collection, organization, presentation, and interpretation of these events. Historians seek knowledge of the past using historical sources such as written documents, oral accounts, art and material artifacts, and ecological markers.[5] History is incomplete and still has debatable mysteries.
-
-History is an academic discipline which uses a narrative to describe, examine, question, and analyze past events, and investigate their patterns of cause and effect.[6][7] Historians debate which narrative best explains an event, as well as the significance of different causes and effects. Historians debate the nature of history as an end in itself, and its usefulness in giving perspective on the problems of the present.[6][8][9][10]
-
-Stories common to a particular culture, but not supported by external sources (such as the tales surrounding King Arthur), are usually classified as cultural heritage or legends.[11][12] History differs from myth in that it is supported by verifiable evidence. However, ancient cultural influences have helped create variant interpretations of the nature of history, which have evolved over the centuries and continue to change today. The modern study of history is wide-ranging, and includes the study of specific regions and certain topical or thematic elements of historical investigation. History is taught as a part of primary and secondary education, and the academic study of history is a major discipline in universities.
-
-Herodotus, a 5th-century BC Greek historian, is often considered the "father of history", as one of the first historians in the Western tradition,[13] though he has been criticized as the "father of lies".[14][15] Along with his contemporary Thucydides, he helped form the foundations for the modern study of past events and societies.[16] Their works continue to be read today, and the gap between the culture-focused Herodotus and the military-focused Thucydides remains a point of contention or approach in modern historical writing. In East Asia, a state chronicle, the Spring and Autumn Annals, was reputed to date from as early as 722 BC, though only 2nd-century BC texts have survived.
-
-Etymology
-
-History by Frederick Dielman (1896)
-The word history comes from historía (Ancient Greek: ἱστορία, romanized: historíā, lit. 'inquiry, knowledge from inquiry, or judge'[17]). It was in that sense that Aristotle used the word in his History of Animals.[18] The ancestor word ἵστωρ is attested early on in Homeric Hymns, Heraclitus, the Athenian ephebes' oath, and in Boeotic inscriptions (in a legal sense, either "judge" or "witness", or similar). The Greek word was borrowed into Classical Latin as historia, meaning "investigation, inquiry, research, account, description, written account of past events, writing of history, historical narrative, recorded knowledge of past events, story, narrative". History was borrowed from Latin (possibly via Old Irish or Old Welsh) into Old English as stær ("history, narrative, story"), but this word fell out of use in the late Old English period.[19] Meanwhile, as Latin became Old French (and Anglo-Norman), historia developed into forms such as istorie, estoire, and historie, with new developments in the meaning: "account of the events of a person's life (beginning of the 12th century), chronicle, account of events as relevant to a group of people or people in general (1155), dramatic or pictorial representation of historical events (c. 1240), body of knowledge relative to human evolution, science (c. 1265), narrative of real or imaginary events, story (c. 1462)".[19]
-
-It was from Anglo-Norman that history was brought into Middle English, and it has persisted. It appears in the 13th-century Ancrene Wisse, but seems to have become a common word in the late 14th century, with an early attestation appearing in John Gower's Confessio Amantis of the 1390s (VI.1383): "I finde in a bok compiled | To this matiere an old histoire, | The which comth nou to mi memoire". In Middle English, the meaning of history was "story" in general. The restriction to the meaning "the branch of knowledge that deals with past events; the formal record or study of past events, esp. human affairs" arose in the mid-15th century.[19] With the Renaissance, older senses of the word were revived, and it was in the Greek sense that Francis Bacon used the term in the late 16th century, when he wrote about natural history. For him, historia was "the knowledge of objects determined by space and time", that sort of knowledge provided by memory (while science was provided by reason, and poetry was provided by fantasy).[20]
-
-In an expression of the linguistic synthetic vs. analytic/isolating dichotomy, English like Chinese (史 vs. 诌) now designates separate words for human history and storytelling in general. In modern German, French, and most Germanic and Romance languages, which are solidly synthetic and highly inflected, the same word is still used to mean both "history" and "story". Historian in the sense of a "researcher of history" is attested from 1531. In all European languages, the substantive history is still used to mean both "what happened with men", and "the scholarly study of the happened", the latter sense sometimes distinguished with a capital letter, or the word historiography.[18][further explanation needed] The adjective historical is attested from 1661, and historic from 1669.[21]
-
-Description
-
-The title page to The Historians' History of the World
-Historians write in the context of their own time, and with due regard to the current dominant ideas of how to interpret the past, and sometimes write to provide lessons for their own society. In the words of Benedetto Croce, "All history is contemporary history". History is facilitated by the formation of a "true discourse of past" through the production of narrative and analysis of past events relating to the human race.[22] The modern discipline of history is dedicated to the institutional production of this discourse.
-
-All events that are remembered and preserved in some authentic form constitute the historical record.[23] The task of historical discourse is to identify the sources which can most usefully contribute to the production of accurate accounts of past. Therefore, the constitution of the historian's archive is a result of circumscribing a more general archive by invalidating the usage of certain texts and documents (by falsifying their claims to represent the "true past"). Part of the historian's role is to skillfully and objectively use the many sources from the past, most often found in the archives. The process of creating a narrative inevitably generates debate, as historians remember or emphasize different events of the past.[24]
-
-The study of history has sometimes been classified as part of the humanities, other times part of the social sciences.[25] It can be seen as a bridge between those two broad areas, incorporating methodologies from both. Some historians strongly support one or the other classification.[26] In the 20th century the Annales school revolutionized the study of history, by using such outside disciplines as economics, sociology, and geography in the study of global history.[27]
-
-Traditionally, historians have recorded events of the past, either in writing or by passing on an oral tradition, and attempted to answer historical questions through the study of written documents and oral accounts. From the beginning, historians have used such sources as monuments, inscriptions, and pictures. In general, the sources of historical knowledge can be separated into three categories: what is written, what is said, and what is physically preserved, and historians often consult all three.[28] But writing is the marker that separates history from what comes before.
-
-Archaeology is especially helpful in unearthing buried sites and objects, which contribute to the study of history. Archeological finds rarely stand
+        <div class="globe">
+        
+<IconContext.Provider value={{ className: "shared-class", size: 70 }}>
+  <>
+    <FaGlobe />
+   Agency
+  </>
+</IconContext.Provider>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find Out More</button>
+          </div>
         </div>
     )
 }
