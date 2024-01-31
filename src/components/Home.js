815df077673a690990dfdf1f636e6d39e31fc167
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-01-31 12:43:56 +0200

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {FaGlobe} from 'react-icons/fa'
+import {FaGlobe,FaEnvelope,FaUser, FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaWhatsapp} from 'react-icons/fa'
 import { IconContext } from "react-icons";
 
 export default function Home () {
     return (
         <div class="container-fluid">
-        <div class="globe">
+        
         
 <IconContext.Provider value={{ className: "shared-class", size: 70 }}>
   <>
@@ -14,7 +14,56 @@ export default function Home () {
   </>
 </IconContext.Provider>
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find Out More</button>
+
+
+          <IconContext.Provider value={{ className: "shared-class", size: 70 }}>
+  <>
+    <FaEnvelope />
+   Contact Us
+  </>
+</IconContext.Provider>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find Out More</button>
+
+                <IconContext.Provider value={{ className: "shared-class", size: 70 }}>
+  <>
+    <FaUser />
+   Who we Are
+  </>
+</IconContext.Provider>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find Out More</button>
+
+          <div class='info'>
+          <div class='infoHeader'>
+            Info/Bio
           </div>
-        </div>
+    Mr Pathan Sas
+    Via del Pilastro 15-6, 40127 Bologna (Bologna)
+    Telephone: +39 3292121210
+    VAT number: 02823441205
+    NETWORK: 148895.reteimprese.org
+    SECTOR: Ecology and Thermotechnics
+    CATEGORY: Cleaning Companies
+    Cookie Preferences
+          </div>
+<div class='shareCompany'>
+Share Company:
+    <FaFacebook/>
+    <FaTwitter/>
+    <FaInstagram/>
+    <FaLinkedin/>
+    <FaWhatsapp/>
+</div>
+<div class='sharePage'>
+    Share Page:
+    <FaFacebook/>
+    <FaTwitter/>
+    <FaInstagram/>
+    <FaLinkedin/>
+    <FaWhatsapp/>
+</div>
+         
+      </div>
+
+
     )
 }
