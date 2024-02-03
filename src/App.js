8678ea4122f51a06d0550952761015088929c105
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-02-03 10:07:23 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Navbar from './Navbar';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
-import Action from './components/Action';
-import Another from './components/Another';
-import Something from './components/Something';
 import ContactUs from './components/ContactUs';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
@@ -21,9 +18,6 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
       <Route path="/ContactUs" element={<ContactUs />} />
-      <Route path="/Action" element={<Action />} />
-      <Route path="/Another" element={<Another />} />
-      <Route path="/Something" element={<Something />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
     </div>
