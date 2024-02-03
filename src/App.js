14d54f7952b13fb61f332aa252e5718f337e32da
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-02-03 10:18:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
-import ContactUs from './components/Contacts';
+import Contacts from './components/Contacts';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
 import {Routes, Route} from 'react-router-dom';
