39c99f96c7887d3dd1bb186962b038340d83e815
AleyaSiddika <aleyasiddika01@gmail.com>
2024-02-03 10:57:31 +0200

    app.js fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './components/About';
 import Contacts from './components/Contacts';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
-import WhereWeAre from './components/WhereWeAre';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -18,11 +17,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
-      <Route path="/ContactUs" element={<ContactUs />} />
-      <Route path="/Action" element={<Action />} />
-      <Route path="/Another" element={<Another />} />
-      <Route path="/Something" element={<Something />} />
-      <Route path="/WhereWeAre" element={<WhereWeAre />} />
       <Route path="/Contacts" element={<Contacts />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
