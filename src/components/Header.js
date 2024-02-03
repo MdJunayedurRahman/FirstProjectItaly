479fd7ea68885fc1382de19c9319f6e16a6d7040
Pankaj Chakrabarty <t1chpa00@students.oamk.fi>
2024-02-03 13:21:18 +0200

    contact page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div id="header">
-      Civil and industrial cleaning. Painting, clearing of cellars and
-      warehouses. Move.
+    
     </div>
   );
 }
