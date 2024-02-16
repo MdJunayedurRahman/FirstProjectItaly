3c6a0215abea2073b4b50324a983280a943f4382
JunayedurRahman <t2bhmd00@students.oamk.fi>
2024-02-16 03:58:45 +0200

    updated about page font size

--- src/components/About.js
+++ src/components/About.js
@@ -11,31 +11,30 @@ export default function About() {
             className="img-fluid"
             style={{ width: "100%", height: "auto" }}
           />
-          <div className="col-lg-6 d-flex align-items-center justify-content-center">
-            <div
-              className="text-success"
-              style={{
-                marginTop: "20px",
-                marginBottom: "30px",
-                fontSize: "20px",
-                fontWeight: "bold",
-                textAlign: "left",
-                marginLeft: "1px",
-                marginRight: "1px",
-              }}
-            >
-              Un team coeso, dinamico e altamente professionale, con oltre 5
-              anni di esperienza, si avvale di un gruppo di professionisti
-              preparati per offrire servizi di pulizia per appartamenti, case,
-              ville, condomini, negozi, uffici e ambienti industriali di
-              qualsiasi dimensione. Inoltre, forniamo servizi di trasloco e
-              trasporto, facchinaggio, svuotamento di cantine, soffitte e
-              garage, smontaggio e montaggio di arredi, con preventivi e
-              sopralluoghi gratuiti. Il nostro obiettivo principale è
-              selezionare i professionisti più qualificati per garantire la
-              massima qualità nei nostri servizi, coprendo una vasta gamma di
-              esigenze.
-            </div>
+        </div>
+        <div className="col-lg-6 d-flex align-items-center justify-content-center">
+          <div
+            className="text-success"
+            style={{
+              marginTop: "20px",
+              marginBottom: "30px",
+              fontSize: "20px",
+              fontWeight: "bold",
+              textAlign: "left",
+              marginLeft: "1px",
+              marginRight: "1px",
+            }}
+          >
+            Un team coeso, dinamico e altamente professionale, con oltre 5 anni
+            di esperienza, si avvale di un gruppo di professionisti preparati
+            per offrire servizi di pulizia per appartamenti, case, ville,
+            condomini, negozi, uffici e ambienti industriali di qualsiasi
+            dimensione. Inoltre, forniamo servizi di trasloco e trasporto,
+            facchinaggio, svuotamento di cantine, soffitte e garage, smontaggio
+            e montaggio di arredi, con preventivi e sopralluoghi gratuiti. Il
+            nostro obiettivo principale è selezionare i professionisti più
+            qualificati per garantire la massima qualità nei nostri servizi,
+            coprendo una vasta gamma di esigenze.
           </div>
         </div>
       </div>
